117125d861d3c8bac2236c155f85cc562074a111
Felipe Marques <fmcdeoliveira@gmail.com>
2024-04-29 00:12:32 -0300

    Remove accidental experimental code left in UndefinedBuildTool.cs

--- Source/Programs/UndefinedBuildTool/UndefinedBuildTool.cs
+++ Source/Programs/UndefinedBuildTool/UndefinedBuildTool.cs
@@ -1,76 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Reflection;
 using System.Text;
 using UndefinedCore;
 
-using Version = UndefinedCore.Version;
-
 namespace UndefinedBuildTool;
-
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
-public class NativeTypeAttribute : Attribute
-{
-   public Guid ClassGuid { get; }
-
-   public NativeTypeAttribute(string classGuid)
-   {
-      ClassGuid = new Guid(classGuid);
-   }
-}
-
-public class NativeTypes
-{
-   private static Dictionary<Guid, Type>? s_NativeTypes;
-
-   public static bool TryGetType(Guid classGuid, [NotNullWhen(true)] out Type? type)
-   {
-      if (s_NativeTypes == null)
-      {
-         s_NativeTypes = [];
-         foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
-         {
-            foreach (Type t in assembly.GetTypes())
-            {
-               if (t.GetCustomAttribute<NativeTypeAttribute>() is NativeTypeAttribute nativeTypeAttribute)
-               {
-                  s_NativeTypes.Add(nativeTypeAttribute.ClassGuid, t);
-               }
-            }
-         }
-      }
-
-      return s_NativeTypes.TryGetValue(classGuid, out type);
-
-   }
-}
-
-[NativeType("dbca19a3-bb1d-4d0e-9cf9-230522850996")]
-public class Project
-{
-   public string Name { get; set; } = string.Empty;
-   public string FilePath { get; set; } = string.Empty;
-}
-
-public static class BinaryReaderExtensions
-{
-   public static Guid ReadGuid(this BinaryReader reader)
-   {
-      long part1 = reader.ReadInt64();
-      long part2 = reader.ReadInt64();
-      Span<byte> guidBytes = stackalloc byte[16];
-      BitConverter.TryWriteBytes(guidBytes, part1);
-      BitConverter.TryWriteBytes(guidBytes[8..], part2);
-      return new Guid(guidBytes);
-   }
-}
-
-public class ProjectWindowSettings
-{
-   public Version? VSCompilerVersion { get; set; }
-}
 
 internal class UndefinedBuildTool
 {
