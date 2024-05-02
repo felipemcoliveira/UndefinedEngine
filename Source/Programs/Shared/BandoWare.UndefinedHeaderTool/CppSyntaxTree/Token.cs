f4cb65cd598ce7bb3a13fa4a2befc6ddb85e726b
Felipe Marques <fmcdeoliveira@gmail.com>
2024-05-02 13:52:32 -0300

    Add DebuggerDisplay to Token

--- Source/Programs/Shared/BandoWare.UndefinedHeaderTool/CppSyntaxTree/Token.cs
+++ Source/Programs/Shared/BandoWare.UndefinedHeaderTool/CppSyntaxTree/Token.cs
@@ -1,11 +1,16 @@
 using BandoWare.Core;
+using System.Diagnostics;
 
 namespace BandoWare.UndefinedHeaderTool.SyntaxTree;
 
+[DebuggerDisplay("{DebuggerDisplay,nq}")]
 public struct Token(TokenType type, int contentPosition, StringView contentView, int index)
 {
    public StringView ValueView { get; private set; } = contentView;
    public int ContentPosition { get; private set; } = contentPosition;
    public TokenType Type { get; private set; } = type;
    public int Index { get; private set; } = index;
+
+   [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+   internal readonly string DebuggerDisplay => $"\"{ValueView}\" ({Type} @ {ContentPosition})";
 }
