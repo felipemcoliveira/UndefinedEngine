5f4f1f34be21299f1a18a001bf5ab3379bf7638a
Felipe Marques <fmcdeoliveira@gmail.com>
2024-05-16 23:41:29 -0300

    Add support for File/DirectoryReference to command line arguments parsing

---
+++ Source/Programs/Shared/BandoWare.Core/CommandLineOptions/FileSystemReferenceParser.cs
@@ -0,0 +1,48 @@
+﻿using BandoWare.Core.FileSystem;
+using System;
+
+namespace BandoWare.Core;
+
+public class FileReferenceParser : CommandArgsParser
+{
+   public override object ParseArgs(CommandLineArguments commandLineArguments, ReadOnlySpan<string> args, Type targetType)
+   {
+      if (args.Length > 1)
+      {
+         throw new CommandLineParseException("Too many arguments.");
+      }
+
+      try
+      {
+         FileReference fileReference = new(args[0]);
+         return fileReference;
+      }
+      catch
+      {
+
+         throw new CommandLineParseException("Invalid file path.");
+      }
+   }
+}
+
+public class DirectoryReferenceParser : CommandArgsParser
+{
+   public override object ParseArgs(CommandLineArguments commandLineArguments, ReadOnlySpan<string> args, Type targetType)
+   {
+      if (args.Length > 1)
+      {
+         throw new CommandLineParseException("Too many arguments.");
+      }
+
+      try
+      {
+         DirectoryReference directoryReference = new(args[0]);
+         return directoryReference;
+      }
+      catch
+      {
+
+         throw new CommandLineParseException("Invalid directory path.");
+      }
+   }
+}
